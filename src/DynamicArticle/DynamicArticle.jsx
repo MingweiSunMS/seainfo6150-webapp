d84ba3ee2974b23b2facfe196f1b00baf8e7fe79
MingweiSunMS <70933031+MingweiSunMS@users.noreply.github.com>
2021-04-12 22:49:17 -0700

    JavaScript Assignment

--- src/DynamicArticle/DynamicArticle.jsx
+++ src/DynamicArticle/DynamicArticle.jsx
@@ -5,7 +5,17 @@ const DynamicArticle = (props) => {
   return (
     <article>
       {/* add extra html with javascript attributes here */}
-      <HTMLText text={props.article.text} />
+      <header>
+        <h1>{props.article.title}</h1>
+        <address>{props.article.author} (<a href={"mailto:" + props.article.authorEmail}>{props.article.authorEmail}</a>)
+        <br/>
+        </address>
+        <time dateTime={props.article.timeStamp}/>
+        <br/>
+        <img src={props.article.image._url} alt={props.article.title}/>
+      </header>
+
+      <HTMLText text={props.article.text}/>
     </article>
   );
 };
